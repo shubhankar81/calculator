3c3a9ce41da69470c183e1158257d19e62bb034e
Shubhankar Kumar <k.shubhankar81@gmail.com>
2020-10-28 11:33:46 +0530

    add minus implementation

--- src/calculator/calculator.java
+++ src/calculator/calculator.java
@@ -2,11 +2,17 @@ package calculator;
 
 public class calculator {
 	
-	int add(String numbers) {
+	public void checkNegative(int number) throws Exception {
+		if(number<0) {
+			throw new Exception("Negatives not Allowed");
+		}
+	}
+	int add(String numbers) throws Exception {
 		int a=0;
 		if(numbers.length() == 1) {
 			try { 
-				a+=Integer.parseInt(numbers);
+				a+= Integer.parseInt(numbers);
+				this.checkNegative(a);
 			}catch(Exception e) {
 				
 			}
@@ -14,6 +20,7 @@ public class calculator {
 			String num[] = numbers.split(",|\n");
 			for(String i: num) {
 				a+=Integer.parseInt(i);
+				this.checkNegative(a);
 			}
 		}else {
 			String b="0";
@@ -22,6 +29,7 @@ public class calculator {
 					b+=numbers.charAt(i);
 				}else{
 					a+= b!= "" ? Integer.parseInt(b) : 0;
+					this.checkNegative(a);
 					b="";
 				}
 				if(i==numbers.length()-1) {
@@ -34,7 +42,7 @@ public class calculator {
 		return numbers.isEmpty() ? 0 : a ;
 	}
 	
-	public static void main(String ar[]) {
+	public static void main(String ar[]) throws Exception {
 		System.out.println("Inside main Block");
 		calculator c = new calculator();
 		System.out.println("ss" +c.add("//;\\n1;2"));
--- src/calculator/calculatorTest.java
+++ src/calculator/calculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -9,32 +10,43 @@ class calculatorTest {
 	calculator c= new calculator();
 	
 	@Test
-	public void testAdd() {
+	public void testAdd() throws Exception {
 		assertEquals(0, c.add(""));
 	}
 	
 	@Test
-	public void singleDigitCheck() {
+	public void singleDigitCheck() throws Exception {
 		assertEquals(3, c.add("3"));
 	}
 	
 	@Test
-	public void testSumWithCommas() {
+	public void testSumWithCommas() throws Exception {
 		assertEquals(4, c.add("1,3"));
 	}
 	
 	@Test
-	public void testWithMultipleCommas() {
+	public void testWithMultipleCommas() throws Exception {
 		assertEquals(60, c.add("1,4,5,6,7,3,2,3,5,7,8,9"));
 	}
 	
 	@Test
-	public void testWithNewLines() {
+	public void testWithNewLines() throws Exception {
 		assertEquals(10, c.add("1,2,3\n4"));
 	}
 	
 	@Test
-	public void testwithDelimeters() {
+	public void testwithDelimeters() throws Exception {
 		assertEquals(30, c.add("//;\\n10;20"));
+	}
+	@Test
+	public void testWithMinus() {
+		Throwable e = null;
+		try {
+			c.add("1,-2,3\n4");
+		}catch(Throwable ex) {
+			e = ex;
+		}
+		assertTrue(e instanceof Exception);
+		
 	}
 }
