abd52793ede3f11bdb2b3d1ee09891ce5dde7df4
Shubhankar Kumar <k.shubhankar81@gmail.com>
2020-10-28 10:25:24 +0530

    New Lines Case Added

--- src/calculator/calculator.java
+++ src/calculator/calculator.java
@@ -3,26 +3,24 @@ package calculator;
 public class calculator {
 
 	int add(String numbers) {
-		int a=0,b=0;
+		int a=0;
 		try { 
 			a = Integer.parseInt(numbers);
 		}catch(Exception e) {}
-		if(numbers.contains(",")) {
-			String num[] = numbers.split(",");
+		if(numbers.contains(",")||numbers.contains("\n")) {
+			String num[] = numbers.split(",|\n");
 			for(String i: num) {
 				a+=Integer.parseInt(i);
 			}
-			//a=Integer.parseInt(num[0]);
-			//b=Integer.parseInt(num[1]);
 		}
 		
-		return numbers.isEmpty() ? 0 : a+b ;
+		return numbers.isEmpty() ? 0 : a ;
 	}
 	
 	public static void main(String ar[]) {
 		System.out.println("Inside main Block");
 		calculator c = new calculator();
-		System.out.println(c.add("1,3,4,5,6,7,8,9,9,2"));
+		System.out.println("ss" +c.add("1,\n"));
 	}
 	
 }
--- src/calculator/calculatorTest.java
+++ src/calculator/calculatorTest.java
@@ -27,4 +27,9 @@ class calculatorTest {
 	public void testWithMultipleCommas() {
 		assertEquals(60, c.add("1,4,5,6,7,3,2,3,5,7,8,9"));
 	}
+	
+	@Test
+	public void testWithNewLines() {
+		assertEquals(10, c.add("1,2,3\n4"));
+	}
 }
