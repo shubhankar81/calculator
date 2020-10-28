1bf94168894e818cef5113595f5a8e4b806a6cd9
Shubhankar Kumar <k.shubhankar81@gmail.com>
2020-10-28 09:43:54 +0530

    multiple numbers with commas

--- src/calculator/calculator.java
+++ src/calculator/calculator.java
@@ -9,8 +9,11 @@ public class calculator {
 		}catch(Exception e) {}
 		if(numbers.contains(",")) {
 			String num[] = numbers.split(",");
-			a=Integer.parseInt(num[0]);
-			b=Integer.parseInt(num[1]);
+			for(String i: num) {
+				a+=Integer.parseInt(i);
+			}
+			//a=Integer.parseInt(num[0]);
+			//b=Integer.parseInt(num[1]);
 		}
 		
 		return numbers.isEmpty() ? 0 : a+b ;
@@ -19,7 +22,7 @@ public class calculator {
 	public static void main(String ar[]) {
 		System.out.println("Inside main Block");
 		calculator c = new calculator();
-		System.out.println(c.add("1"));
+		System.out.println(c.add("1,3,4,5,6,7,8,9,9,2"));
 	}
 	
 }
